953c39591aea52a2befc8b0e55381bc121b87fa4
Aleksey <56933174+AlekseiZhukov@users.noreply.github.com>
2020-06-22 22:09:47 +0300

    Add files via upload

---
+++ precache-manifest.d7b69623fae1cdb86e7efcc039751e16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7211b1c349974001725a97b3d330841d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffad6385e5664c8b6100",
+    "url": "/static/css/main.47be4583.chunk.css"
+  },
+  {
+    "revision": "0eba957e81cc8d54faab",
+    "url": "/static/js/2.3f038502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3f038502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffad6385e5664c8b6100",
+    "url": "/static/js/main.ddd1e232.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "e505d02bc4763dc0edd0e33fc04dc9cb",
+    "url": "/static/media/basket.e505d02b.svg"
+  }
+]);
